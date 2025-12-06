b218687dc6a3240f2662246747b88f50c4357196
CodeVryn <daniil@mashkovtsev.org>
2025-12-06 19:07:54 +0400

    Setup alembic to autogenerate migrations

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -7,6 +7,9 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.core.config.settings import get_settings
+from app.core.database.db_models.base import Base
+from app.core.database.db_models import stock_price  # noqa: F401
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,9 +26,7 @@ config.set_main_option('sqlalchemy.url', settings.database.url_async)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

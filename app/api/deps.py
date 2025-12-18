6f4eaa6de31b829ef92b7307b9929298a99e8614
Max Elnikov <elnikow.max@gmail.com>
2025-12-18 16:16:09 +0300

    Added docstrings

--- app/api/deps.py
+++ app/api/deps.py
@@ -16,6 +16,14 @@ settings = get_settings()
 
 
 async def authenticate_user(credentials: CredsDep) -> UserSchema:
+    """Create UserSchema from BasicAuth credentials, also check if credentials are match admin creds
+
+    Args:
+        credentials (CredsDep): BasicAuth credentials
+
+    Returns:
+        UserSchema: Schema representing user
+    """
     username, password = credentials.username, credentials.password
     is_admin = username == settings.jwt.admin_username and verify_password(password, settings.jwt.admin_password_hash)
     user = UserSchema(
@@ -27,6 +35,17 @@ async def authenticate_user(credentials: CredsDep) -> UserSchema:
 
 
 async def get_token_payload(credentials: TokenDep) -> TokenPayload:
+    """Extract payload from token
+
+    Args:
+        credentials (TokenDep): AuthorizationCredentials with token
+
+    Raises:
+        HTTPException: If token is invalid
+
+    Returns:
+        TokenPayload: Decoded token payload
+    """
     token = credentials.credentials
     try:
         payload = decode_jwt(token=token)
@@ -36,7 +55,20 @@ async def get_token_payload(credentials: TokenDep) -> TokenPayload:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
 
 
-async def require_admin(token_payload: TokenPayload = Depends(get_token_payload)) -> TokenPayload:
+async def require_admin(token_payload: Annotated[TokenPayload, Depends(get_token_payload)]) -> TokenPayload:
+    """
+    Dependency to check if user is admin
+    Check is_admin field in token payload
+
+    Args:
+        token_payload (Annotated[TokenPayload, Depends): Decoded token payload
+
+    Raises:
+        HTTPException: If user is not admin
+
+    Returns:
+        TokenPayload: If user is admin
+    """
     if not token_payload.is_admin:
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Insufficient permissions')
     return token_payload
--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -9,6 +9,9 @@ router = APIRouter(prefix='/jwt', tags=['JWT'])
 
 @router.post('/login', response_model=TokenInfo)
 async def login_access_token(user: UserSchema = Depends(authenticate_user)):
+    """
+    Generate JWT token for future requests
+    """
     jwt_payload = {'sub': user.username, 'is_admin': user.is_admin}
     token = encode_jwt(jwt_payload)
     return TokenInfo(access_token=token, token_type='Bearer')

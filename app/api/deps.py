0252190b6601194ce2c4100dd8bf7357b4ae399e
Max Elnikov <elnikow.max@gmail.com>
2025-12-18 13:57:07 +0300

    Added auth dependencies

---
+++ app/api/deps.py
@@ -0,0 +1,40 @@
+from typing import Annotated
+
+from fastapi import Body, Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jwt import InvalidTokenError
+from pydantic import ValidationError
+
+from app.config.settings import get_settings
+from app.core.auth.utils import decode_jwt, verify_password
+from app.core.schemas.jwt_auth import TokenPayload, UserSchema
+
+CredsDep = Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())]
+
+settings = get_settings()
+
+
+async def authenticate_user(username: str = Body(), password: str = Body()) -> UserSchema:
+    is_admin = username == settings.jwt.admin_username and verify_password(password, settings.jwt.admin_password_hash)
+    user = UserSchema(
+        username=username,
+        password=password,
+        is_admin=is_admin,
+    )
+    return user
+
+
+async def get_token_payload(credentials: CredsDep) -> TokenPayload:
+    token = credentials.credentials
+    try:
+        payload = decode_jwt(token=token)
+        return TokenPayload(**payload)
+
+    except InvalidTokenError or ValidationError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+
+
+async def require_admin(token_payload: TokenPayload = Depends(get_token_payload)) -> TokenPayload:
+    if not token_payload.is_admin:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Insufficient permissions')
+    return token_payload

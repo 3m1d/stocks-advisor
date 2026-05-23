d8f5abd725a5d4fc258bd2def8effb2876a7a6f0
CodeVryn <daniil@mashkovtsev.org>
2026-05-24 01:42:58 +0400

    Add ticker TCSG group

--- app/core/processors/stocks_parser.py
+++ app/core/processors/stocks_parser.py
@@ -47,7 +47,7 @@ class StocksParser:
 
     async def parse_stocks(self):
         return await self.moex_client.get_data(
-            tickers=[Ticker.Gazprom, Ticker.Rosneft, Ticker.Lukoil, Ticker.Sber],
+            tickers=[Ticker.Gazprom, Ticker.Rosneft, Ticker.Lukoil, Ticker.Sber, Ticker.TCSGroup],
             engine=Engines.STOCK,
             market=Markets.SHARES,
             board=Boards.TQBR,

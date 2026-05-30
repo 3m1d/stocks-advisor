fa37f5c2cca0bbc868f0441c9062cd9aec328d6c
CodeVryn <daniil@mashkovtsev.org>
2026-05-30 11:15:23 +0400

    News processor as class

--- app/core/processors/news_process.py
+++ app/core/processors/news_process.py
@@ -1,6 +1,5 @@
 import re
 
-import click
 import nltk
 import pandas as pd
 from natasha import Doc, MorphVocab, NewsEmbedding, NewsMorphTagger, NewsNERTagger, NewsSyntaxParser, Segmenter
@@ -10,295 +9,287 @@ from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 from transformers import pipeline
 
-# Словарь ключевых слов для тикеров голубых фишек
-TICKER_KEYWORDS = {
-    'SBER': ['сбербанк', 'сбер', 'пао сбербанк'],
-    'GAZP': ['газпром', 'пао газпром', 'газовая компания'],
-    'LKOH': ['лукойл', 'пао лукойл', 'нк лукойл'],
-    'ROSN': ['роснефть', 'пао роснефть', 'нк роснефть'],
-    'YDEX': ['яндекс', 'yandex', 'яндекс.такси', 'яндекс.недвижимость'],
-    'VTBR': ['втб', 'банк втб', 'пао втб'],
-    'TATN': ['татнефть', 'пао татнефть', 'татнефть-нк'],
-    'GMKN': ['норильский никель', 'пао гмк норильский никель', 'gmk norilsk nickel'],
-}
 
-# Ключевые слова по индексам
-INDEX_KEYWORDS = {
-    'MOEXOG': [  # Нефть и газ
-        'нефть',
-        'газ',
-        'добыча',
-        'бурение',
-        'скважина',
-        'топливо',
-        'трубопровод',
-        'нефтепереработка',
-        'нефтехимия',
-        'сланец',
-        'бензин',
-        'дизель',
-        'нефтяная компания',
-    ],
-    'MOEXFN': [  # Финансовый сектор
-        'банк',
-        'кредит',
-        'ставка',
-        'ипотека',
-        'вклад',
-        'депозит',
-        'облигация',
-        'акция',
-        'финансы',
-        'страхование',
-        'инвестиции',
-        'ЦБ',
-        'биржа',
-        'дивиденды',
-        'брокер',
-    ],
-    'MOEXMM': [  # Металлургия и добыча
-        'металл',
-        'сталь',
-        'рудник',
-        'шахта',
-        'добыча',
-        'уголь',
-        'железная руда',
-        'никель',
-        'медь',
-        'алюминий',
-        'золото',
-        'серебро',
-        'горнодобыча',
-        'плавка',
-    ],
-    'MOEXCN': [  # Потребительский сектор
-        'потребитель',
-        'ритейл',
-        'магазин',
-        'торговля',
-        'продукты',
-        'еда',
-        'напитки',
-        'одежда',
-        'FMCG',
-        'бытовая техника',
-        'супермаркет',
-        'розничная торговля',
-        'товары',
-    ],
-    'MOEXTN': [  # Транспорт
-        'транспорт',
-        'логистика',
-        'авиакомпания',
-        'железная дорога',
-        'порт',
-        'флот',
-        'аэропорт',
-        'автоперевозки',
-        'грузоперевозки',
-        'контейнер',
-        'трамвай',
-        'метро',
-    ],
-    'MOEXEU': [  # Электроэнергетика
-        'электроэнергия',
-        'энергетика',
-        'генерация',
-        'электростанция',
-        'теплоэнергия',
-        'ГЭС',
-        'ТЭЦ',
-        'АЭС',
-        'мощность',
-        'энергосбыт',
-        'электричество',
-        'энергокомпания',
-    ],
-    'MOEXTL': [  # Телекоммуникации
-        'телеком',
-        'связь',
-        'интернет',
-        'мобильный оператор',
-        'сотовая связь',
-        'телефон',
-        'передача данных',
-        'оптоволокно',
-        'трафик',
-        'сетевые технологии',
-    ],
-    'MOEXCH': [  # Химия и нефтехимия
-        'химия',
-        'нефтехимия',
-        'пластик',
-        'полимер',
-        'удобрения',
-        'реактивы',
-        'синтез',
-        'нефтепродукты',
-        'катализатор',
-        'химическое производство',
-        'серная кислота',
-    ],
-    'MOEXIT': [  # Информационные технологии
-        'it',
-        'айти',
-        'технологии',
-        'программное обеспечение',
-        'разработка',
-        'интернет-сервис',
-        'стартап',
-        'искусственный интеллект',
-        'кибербезопасность',
-        'софт',
-        'облачные технологии',
-        'приложение',
-        'данные',
-        'автоматизация',
-        'цифровизация',
-    ],
-    'MOEXRE': [  # Строительные компании
-        'стройка',
-        'строительство',
-        'девелопмент',
-        'недвижимость',
-        'жилой комплекс',
-        'инфраструктура',
-        'ремонт',
-        'архитектура',
-        'подрядчик',
-        'застройщик',
-        'объект',
-        'капитальное строительство',
-    ],
-}
+class NewsProcessor:
+    # Словарь ключевых слов для тикеров голубых фишек
+    TICKER_KEYWORDS = {
+        'SBER': ['сбербанк', 'сбер', 'пао сбербанк'],
+        'GAZP': ['газпром', 'пао газпром', 'газовая компания'],
+        'LKOH': ['лукойл', 'пао лукойл', 'нк лукойл'],
+        'ROSN': ['роснефть', 'пао роснефть', 'нк роснефть'],
+        'YDEX': ['яндекс', 'yandex', 'яндекс.такси', 'яндекс.недвижимость'],
+        'VTBR': ['втб', 'банк втб', 'пао втб'],
+        'TATN': ['татнефть', 'пао татнефть', 'татнефть-нк'],
+        'GMKN': ['норильский никель', 'пао гмк норильский никель', 'gmk norilsk nickel'],
+    }
 
+    # Ключевые слова по индексам
+    INDEX_KEYWORDS = {
+        'MOEXOG': [  # Нефть и газ
+            'нефть',
+            'газ',
+            'добыча',
+            'бурение',
+            'скважина',
+            'топливо',
+            'трубопровод',
+            'нефтепереработка',
+            'нефтехимия',
+            'сланец',
+            'бензин',
+            'дизель',
+            'нефтяная компания',
+        ],
+        'MOEXFN': [  # Финансовый сектор
+            'банк',
+            'кредит',
+            'ставка',
+            'ипотека',
+            'вклад',
+            'депозит',
+            'облигация',
+            'акция',
+            'финансы',
+            'страхование',
+            'инвестиции',
+            'ЦБ',
+            'биржа',
+            'дивиденды',
+            'брокер',
+        ],
+        'MOEXMM': [  # Металлургия и добыча
+            'металл',
+            'сталь',
+            'рудник',
+            'шахта',
+            'добыча',
+            'уголь',
+            'железная руда',
+            'никель',
+            'медь',
+            'алюминий',
+            'золото',
+            'серебро',
+            'горнодобыча',
+            'плавка',
+        ],
+        'MOEXCN': [  # Потребительский сектор
+            'потребитель',
+            'ритейл',
+            'магазин',
+            'торговля',
+            'продукты',
+            'еда',
+            'напитки',
+            'одежда',
+            'FMCG',
+            'бытовая техника',
+            'супермаркет',
+            'розничная торговля',
+            'товары',
+        ],
+        'MOEXTN': [  # Транспорт
+            'транспорт',
+            'логистика',
+            'авиакомпания',
+            'железная дорога',
+            'порт',
+            'флот',
+            'аэропорт',
+            'автоперевозки',
+            'грузоперевозки',
+            'контейнер',
+            'трамвай',
+            'метро',
+        ],
+        'MOEXEU': [  # Электроэнергетика
+            'электроэнергия',
+            'энергетика',
+            'генерация',
+            'электростанция',
+            'теплоэнергия',
+            'ГЭС',
+            'ТЭЦ',
+            'АЭС',
+            'мощность',
+            'энергосбыт',
+            'электричество',
+            'энергокомпания',
+        ],
+        'MOEXTL': [  # Телекоммуникации
+            'телеком',
+            'связь',
+            'интернет',
+            'мобильный оператор',
+            'сотовая связь',
+            'телефон',
+            'передача данных',
+            'оптоволокно',
+            'трафик',
+            'сетевые технологии',
+        ],
+        'MOEXCH': [  # Химия и нефтехимия
+            'химия',
+            'нефтехимия',
+            'пластик',
+            'полимер',
+            'удобрения',
+            'реактивы',
+            'синтез',
+            'нефтепродукты',
+            'катализатор',
+            'химическое производство',
+            'серная кислота',
+        ],
+        'MOEXIT': [  # Информационные технологии
+            'it',
+            'айти',
+            'технологии',
+            'программное обеспечение',
+            'разработка',
+            'интернет-сервис',
+            'стартап',
+            'искусственный интеллект',
+            'кибербезопасность',
+            'софт',
+            'облачные технологии',
+            'приложение',
+            'данные',
+            'автоматизация',
+            'цифровизация',
+        ],
+        'MOEXRE': [  # Строительные компании
+            'стройка',
+            'строительство',
+            'девелопмент',
+            'недвижимость',
+            'жилой комплекс',
+            'инфраструктура',
+            'ремонт',
+            'архитектура',
+            'подрядчик',
+            'застройщик',
+            'объект',
+            'капитальное строительство',
+        ],
+    }
 
-def normalize_text(text: str) -> str:
-    """Лемматизация и очистка текста от чисел и символов."""
-    doc = Doc(text)
-    doc.segment(segmenter)
-    doc.tag_morph(morph_tagger)
-    if not doc.tokens:
-        return ''
-    lemmas = []
-    for token in doc.tokens:
-        token.lemmatize(morph_vocab)
-        if re.match(r'^[а-яА-Яa-zA-Z]+$', token.text):  # исключаем числа и знаки
-            lemmas.append(token.lemma.lower())
+    def __init__(self):
+        tqdm.pandas()
+        nltk.download('stopwords')
 
-    return ' '.join(lemmas)
+        # Инициализация моделей Natasha
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        self.emb = NewsEmbedding()
+        self.morph_tagger = NewsMorphTagger(self.emb)
+        self.syntax_parser = NewsSyntaxParser(self.emb)
+        self.ner_tagger = NewsNERTagger(self.emb)
 
+        # Подготовка для векторизации
+        self.russian_stopwords = stopwords.words('russian')
 
-# Инициализация модели для определения тональности
-sentiment_model = pipeline(task='text-classification', model='mxlcw/rubert-tiny2-russian-economic-sentiment')
+        self.sector_names = list(self.INDEX_KEYWORDS.keys())  # список индексов секторов
+        self.sector_docs = [' '.join(words) for words in self.INDEX_KEYWORDS.values()]  # вектор ключевых слов
+        self.sector_docs_norm = [self.normalize_text(doc) for doc in self.sector_docs]  # нормализация вектора
 
+        vectorizer = TfidfVectorizer(stop_words=self.russian_stopwords)
+        self.sector_matrix = vectorizer.fit_transform(self.sector_docs_norm)
 
-def get_text_sentiment(news_texts: list[str]) -> list[str | None]:
-    """
-    Принимает на вход список текстов, применяет модель для определения тональности.
-    Выделяет только класс новости (без уверенности) и возврашает список меток той же размерности
+    def normalize_text(self, text: str) -> str:
+        """Лемматизация и очистка текста от чисел и символов."""
+        doc = Doc(text)
+        doc.segment(self.segmenter)
+        doc.tag_morph(self.morph_tagger)
+        if not doc.tokens:
+            return ''
+        lemmas = []
+        for token in doc.tokens:
+            token.lemmatize(self.morph_vocab)
+            if re.match(r'^[а-яА-Яa-zA-Z]+$', token.text):  # исключаем числа и знаки
+                lemmas.append(token.lemma.lower())
 
-    Args:
-        news_texts (list[str]): Список новостей
+        return ' '.join(lemmas)
 
-    Returns:
-        _type_: Список меток(label) тональностей для новостей
-    """
-    return [sentiment.get('label') for sentiment in tqdm(sentiment_model(news_texts))]
+    # Инициализация модели для определения тональности
+    sentiment_model = pipeline(task='text-classification', model='mxlcw/rubert-tiny2-russian-economic-sentiment')
 
+    def get_text_sentiment(self, news_texts: list[str]) -> list[str | None]:
+        """
+        Принимает на вход список текстов, применяет модель для определения тональности.
+        Выделяет только класс новости (без уверенности) и возврашает список меток той же размерности
 
-# Инициализация моделей Natasha
-segmenter = Segmenter()
-morph_vocab = MorphVocab()
-emb = NewsEmbedding()
-morph_tagger = NewsMorphTagger(emb)
-syntax_parser = NewsSyntaxParser(emb)
-ner_tagger = NewsNERTagger(emb)
+        Args:
+            news_texts (list[str]): Список новостей
 
+        Returns:
+            _type_: Список меток(label) тональностей для новостей
+        """
+        return [sentiment.get('label') for sentiment in tqdm(self.sentiment_model(news_texts))]
 
-# Подготовка для векторизации
-russian_stopwords = stopwords.words('russian')
+    def extract_organizations(self, text: str) -> list[str]:
+        """Извлекает организации из текста с помощью Natasha NER."""
+        doc = Doc(text)
+        doc.segment(self.segmenter)
+        doc.parse_syntax(self.syntax_parser)
+        doc.tag_morph(self.morph_tagger)
+        doc.tag_ner(self.ner_tagger)
+        found_spans = []
+        if not doc.spans:
+            return []
+        for span in doc.spans:
+            if span.type == 'ORG':
+                span.normalize(self.morph_vocab)
+                found_spans.append(span.normal.lower())
+        return found_spans
 
+    def extract_tickers(self, orgs: list[str]) -> list[str]:
+        """Определяет тикеры по извлечённым названиям компаний."""
+        tickers = set()
+        for org in orgs:
+            for ticker, keywords in self.TICKER_KEYWORDS.items():
+                if any(org == kw.lower() for kw in keywords):
+                    tickers.add(ticker)
+        return list(tickers)
 
-sector_names = list(INDEX_KEYWORDS.keys())  # список индексов секторов
-sector_docs = [' '.join(words) for words in INDEX_KEYWORDS.values()]  # вектор ключевых слов
-sector_docs_norm = [normalize_text(doc) for doc in sector_docs]  # нормализация вектора
+    def detect_sector_tfidf(self, text: str) -> str | None:
+        """
+        Определяет сектор новости по косинусному сходству с TF-IDF.
+        Если нет явного сходства - возвращает None
+        """
+        text_norm = self.normalize_text(text)
+        text_vector = self.vectorizer.transform([text_norm])
+        sims = cosine_similarity(text_vector, self.sector_matrix)[0]
+        if sims.max() < 0.1:
+            return None
+        return self.sector_names[sims.argmax()]
 
-vectorizer = TfidfVectorizer(stop_words=russian_stopwords)
-sector_matrix = vectorizer.fit_transform(sector_docs_norm)
+    def get_tickers(self, text: str) -> list[str]:
+        """Верхнеуровневая функция для поиска тикеров
 
+        Args:
+            text (str): текст новости
 
-def extract_organizations(text: str) -> list[str]:
-    """Извлекает организации из текста с помощью Natasha NER."""
-    doc = Doc(text)
-    doc.segment(segmenter)
-    doc.parse_syntax(syntax_parser)
-    doc.tag_morph(morph_tagger)
-    doc.tag_ner(ner_tagger)
-    found_spans = []
-    if not doc.spans:
-        return []
-    for span in doc.spans:
-        if span.type == 'ORG':
-            span.normalize(morph_vocab)
-            found_spans.append(span.normal.lower())
-    return found_spans
+        Returns:
+            list[str]: список найденных тикеров
+        """
+        orgs = self.extract_organizations(text)
+        return self.extract_tickers(orgs)
 
+    def process_news(self, df: pd.DataFrame, out_file: str):
+        print('Ищем тикеры в новостях...')
+        df['tickers'] = df['text'].progress_apply(self.get_tickers)
+        print('DONE!\n')
 
-def extract_tickers(orgs: list[str]) -> list[str]:
-    """Определяет тикеры по извлечённым названиям компаний."""
-    tickers = set()
-    for org in orgs:
-        for ticker, keywords in TICKER_KEYWORDS.items():
-            if any(org == kw.lower() for kw in keywords):
-                tickers.add(ticker)
-    return list(tickers)
+        print('Определяем сектор влияния новостей...')
+        df['sector'] = df['text'].progress_apply(self.detect_sector_tfidf)
+        print('DONE!\n')
 
+        print('Определяем тональность новостей...')
+        df['text_sentiment'] = self.get_text_sentiment(df.text.to_list())
+        print('DONE!\n')
 
-def detect_sector_tfidf(text: str) -> str | None:
-    """
-    Определяет сектор новости по косинусному сходству с TF-IDF.
-    Если нет явного сходства - возвращает None
-    """
-    text_norm = normalize_text(text)
-    text_vector = vectorizer.transform([text_norm])
-    sims = cosine_similarity(text_vector, sector_matrix)[0]
-    if sims.max() < 0.1:
-        return None
-    return sector_names[sims.argmax()]
-
-
-def get_tickers(text: str) -> list[str]:
-    """Верхнеуровневая функция для поиска тикеров
-
-    Args:
-        text (str): текст новости
-
-    Returns:
-        list[str]: список найденных тикеров
-    """
-    orgs = extract_organizations(text)
-    return extract_tickers(orgs)
-
-
-def run_news_pipeline(df: pd.DataFrame, out_file: str):
-    tqdm.pandas()
-    nltk.download('stopwords')
-
-    print('Ищем тикеры в новостях...')
-    df['tickers'] = df['text'].progress_apply(get_tickers)
-    print('DONE!\n')
-
-    print('Определяем сектор влияния новостей...')
-    df['sector'] = df['text'].progress_apply(detect_sector_tfidf)
-    print('DONE!\n')
-
-    print('Определяем тональность новостей...')
-    df['text_sentiment'] = get_text_sentiment(df.text.to_list())
-    print('DONE!\n')
-
-    # сохраняем итоговый файл
-    print('Cохраняем итоговый файл...')
-    df.to_csv(out_file, index=False, na_rep='NULL')
-    print('DONE!')
+        # сохраняем итоговый файл
+        print('Cохраняем итоговый файл...')
+        df.to_csv(out_file, index=False, na_rep='NULL')
+        print('DONE!')
